c0117a74ffd384f9334f77f806b5c24db2e2e29d
Shiroha <86834876+uclk@users.noreply.github.com>
2023-02-20 00:34:24 +0000

    Create uclzoom.user.js

---
+++ file/uclzoom.user.js
@@ -0,0 +1,16 @@
+/* ==UserStyle==
+@name           UCL Zoom
+@namespace      github.com/openstyles/stylus
+@version        1.0.0
+@description    A new userstyle
+@author         Me
+==/UserStyle== */
+
+@-moz-document domain("ucl.zoom.us") {
+    .video-js .vjs-text-track-display.font-16 .vjs-text-track-cue {
+        font-size: 29px!important;}
+    .video-js .vjs-text-track-display.font-20 .vjs-text-track-cue{
+        font-size: 29px!important;}
+    .video-js .vjs-text-track-display .vjs-text-track-cue>div {    
+        background-color: rgba(0,0,0,.375)!important;}
+}
